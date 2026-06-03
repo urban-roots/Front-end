c45b2b33a554bd367197cc7b7158634cc46e7823
Jair4x <Jair4x@gmail.com>
2026-06-02 22:38:35 -0300

    Fix de script
    Ahora el tema de la página carga como debería.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-const tema = localStorage.getItem('tema');
-
-if (tema) {
-    document.body.setAttribute('data-theme', tema);
-} else {
-    document.body.setAttribute('data-theme', 'mural-dark');
-    localStorage.setItem('tema', 'mural-dark');
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const tema = localStorage.getItem('tema');
+    if (tema) {
+        document.body.setAttribute('data-theme', tema);
+    } else {
+        document.body.setAttribute('data-theme', 'mural-dark');
+        localStorage.setItem('tema', 'mural-dark');
+    }
+});
