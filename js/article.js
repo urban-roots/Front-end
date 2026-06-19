0b0b15d8a791836e38c42394db21c5714965dc71
Jair4x <Jair4x@gmail.com>
2026-06-18 21:10:07 -0300

    Ajuste a carga de datos
    Arreglado typo en el nombre de un ID

--- js/article.js
+++ js/article.js
@@ -22,7 +22,7 @@ async function loadArticle() {
         document.getElementById('article-titulo').textContent = articulo.titulo;
         document.getElementById('article-autor').textContent = `Por ${articulo.autor}`;
         document.getElementById('article-cuerpo').innerHTML = articulo.cuerpo;
-        document.getElementById('article-sobre-autor').textContent = articulo.sobreElAutor;
+        document.getElementById('sobre-el-autor').textContent = articulo.sobreElAutor;
         document.querySelector('.article-banner').style.backgroundImage = `url('../img/articulo-${id}.png')`;
 
 
